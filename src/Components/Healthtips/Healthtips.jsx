727b1eccf28de5c4790ba5f620627306a602696e
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-12 18:30:27 +0530

    initial status

---
+++ src/Components/Healthtips/Healthtips.jsx
@@ -0,0 +1,27 @@
+import "./HealthTips.css";
+
+function HealthTips() {
+  return (
+    <div className="tips">
+
+      <h1>Health Tips</h1>
+
+      <ul>
+
+        <li>🥗 Eat Healthy Food</li>
+
+        <li>💧 Drink Plenty of Water</li>
+
+        <li>🚶 Exercise Daily</li>
+
+        <li>😴 Sleep 8 Hours</li>
+
+        <li>🩺 Regular Health Checkup</li>
+
+      </ul>
+
+    </div>
+  );
+}
+
+export default HealthTips;
