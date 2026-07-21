bddb57ef7505e5a7019f54e08f1f341eb33c65f3
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-21 20:30:28 +0530

    initial Frontend

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,23 @@
 import axios from "axios";
 
+// Change this URL after deploying your backend
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
+
+// Automatically attach JWT token to every request
+API.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
