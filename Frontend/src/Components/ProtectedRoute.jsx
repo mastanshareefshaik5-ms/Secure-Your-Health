a634404c70dde873d28798806eeb9b26e675494c
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-21 20:32:01 +0530

    initial Frontend

--- Frontend/src/Components/ProtectedRoute.jsx
+++ Frontend/src/Components/ProtectedRoute.jsx
@@ -1,14 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+function ProtectedRoute({ children }) {
 
   const token = localStorage.getItem("token");
 
   if (!token) {
+
     return <Navigate to="/login" replace />;
+
   }
 
   return children;
-};
+
+}
 
 export default ProtectedRoute;
