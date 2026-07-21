491b95efd70ef181eb3d6860f1b8d75795e64468
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-21 20:25:07 +0530

    initial Backend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,14 +1,92 @@
-const express = require("express");
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-const {
-  register,
-  login,
-} = require("../controllers/authController");
+// Register
+router.post("/register", async (req, res) => {
 
-router.post("/register", register);
+  try {
 
-router.post("/login", login);
+    const { name, email, password } = req.body;
 
-module.exports = router;
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Registration Successful",
+      user,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+});
+
+// Login
+router.post("/login", async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res.status(400).json({
+        message: "Invalid Email",
+      });
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match)
+      return res.status(400).json({
+        message: "Invalid Password",
+      });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.json({
+      token,
+      user,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+});
+
+export default router;
