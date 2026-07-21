b1ef7fe0a2fb4dd38432ee791159e406bcdd09ff
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-21 20:25:42 +0530

    initial Backend

--- Backend/routes/appointmentRoutes.js
+++ Backend/routes/appointmentRoutes.js
@@ -1,16 +1,24 @@
-const express = require("express");
+import express from "express";
+import Appointment from "../models/Appointment.js";
+import protect from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-const {
-  getAppointments,
-  createAppointment,
-  updateAppointment,
-  deleteAppointment,
-} = require("../controllers/appointmentController");
+router.get("/", protect, async (req, res) => {
+  res.json(await Appointment.find());
+});
 
-router.get("/", getAppointments);
-router.post("/", createAppointment);
-router.put("/:id", updateAppointment);
-router.delete("/:id", deleteAppointment);
+router.post("/", protect, async (req, res) => {
+  res.json(await Appointment.create(req.body));
+});
 
-module.exports = router;
+router.put("/:id", protect, async (req, res) => {
+  res.json(await Appointment.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+router.delete("/:id", protect, async (req, res) => {
+  await Appointment.findByIdAndDelete(req.params.id);
+  res.json({ message: "Appointment Deleted" });
+});
+
+export default router;
