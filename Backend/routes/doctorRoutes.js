91f40e2b36e1071a96565b00743638986ae8b4ad
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-21 20:23:00 +0530

    initial Backend

--- Backend/routes/doctorRoutes.js
+++ Backend/routes/doctorRoutes.js
@@ -1,16 +1,24 @@
-const express = require("express");
+import express from "express";
+import Doctor from "../models/Doctor.js";
+import protect from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-const {
-  getDoctors,
-  createDoctor,
-  updateDoctor,
-  deleteDoctor,
-} = require("../controllers/doctorController");
+router.get("/", protect, async (req, res) => {
+  res.json(await Doctor.find());
+});
 
-router.get("/", getDoctors);
-router.post("/", createDoctor);
-router.put("/:id", updateDoctor);
-router.delete("/:id", deleteDoctor);
+router.post("/", protect, async (req, res) => {
+  res.json(await Doctor.create(req.body));
+});
 
-module.exports = router;
+router.put("/:id", protect, async (req, res) => {
+  res.json(await Doctor.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+router.delete("/:id", protect, async (req, res) => {
+  await Doctor.findByIdAndDelete(req.params.id);
+  res.json({ message: "Doctor Deleted" });
+});
+
+export default router;
